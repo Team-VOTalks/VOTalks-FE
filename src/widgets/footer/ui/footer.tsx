857e45f34e15a53dc864592b3ec128df578e6bdb
ChanLee_KR <cl700120@gmail.com>
2024-04-05 18:37:44 +0900

    design : footer 여백 수정

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <ul className="mx-auto mt-6 h-auto w-full max-w-5xl border-t pb-9 pt-6 text-sm text-gray-500">
+      <ul className="mx-auto mt-6 h-auto w-full max-w-5xl border-t px-2 pb-9 pt-6 text-sm text-gray-500">
         <li className="break-keep text-center sm:text-left">
           &copy; 2024. Team-VOTalks all rights reserved.
         </li>
