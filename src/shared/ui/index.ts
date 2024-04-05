ec59086e25d89ac9c4518eccc12c59335137c89a
ChanLee_KR <cl700120@gmail.com>
2024-04-05 11:44:54 +0900

    add : 공용 아이콘 컴포넌트 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,5 +3,29 @@ import IconMoon from './icon-moon';
 import IconSun from './icon-sun';
 import IconPlus from './icon-plus';
 import NotFound from './not-found';
+import IconCheck from './icon-check';
+import IconComment from './icon-comment';
+import IconDelete from './icon-delete';
+import IconDislike from './icon-dislike';
+import IconLike from './icon-like';
+import IconSearch from './icon-search';
+import IconSend from './icon-send';
+import IconShare from './icon-share';
+import IconHome from './icon-home';
 
-export { NotFound, IconPlus, IconSun, IconMoon, IconDropdownAppearance };
+export {
+  NotFound,
+  IconPlus,
+  IconSun,
+  IconMoon,
+  IconDropdownAppearance,
+  IconCheck,
+  IconComment,
+  IconDelete,
+  IconDislike,
+  IconLike,
+  IconSearch,
+  IconSend,
+  IconShare,
+  IconHome,
+};
