3c4ecc84c12bab35aa9623187d4bed19ebe84b4b
ChanLee_KR <cl700120@gmail.com>
2024-04-08 11:05:06 +0900

    remove : 정치 카테고리 삭제

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -3,7 +3,6 @@ export const ADMIN_EMAIL = 'chanlee1007@naver.com';
 export const COMMUNITY_CATEGORIES: { [id: string]: string } = {
   dev: '개발',
   date: '연애',
-  politics: '정치',
   daily: '일상',
   company: '회사',
   friend: '친구',
