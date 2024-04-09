f8f4088ee4dfa3c679ae58f20c870c98984ef452
ChanLee_KR <cl700120@gmail.com>
2024-04-09 14:03:32 +0900

    chore : 카테고리 정책 수정

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,10 +1,10 @@
 export const ADMIN_EMAIL = 'chanlee1007@naver.com';
 
 export const COMMUNITY_CATEGORIES: { [id: string]: string } = {
-  dev: '개발',
   date: '연애',
   daily: '일상',
   company: '회사',
+  dev: '개발',
   friend: '친구',
   family: '가족',
 };
