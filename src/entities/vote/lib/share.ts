3d51833d75967a82d11e7303996c02abfe70bae2
ChanLee_KR <cl700120@gmail.com>
2024-04-12 19:55:24 +0900

    feat : 투표 링크 공유 함수 추가

---
+++ src/entities/vote/lib/share.ts
@@ -0,0 +1,24 @@
+'use client';
+
+type ReturnTypeOfShare = { type: 'clipboard' | 'share' | 'none'; status: boolean };
+
+export default async function share(title: string): Promise<ReturnTypeOfShare> {
+  if (!!navigator?.share) {
+    await navigator.share({
+      title,
+      text: '투표에 참여 후 여러분만의 솔직한 의견을 남겨주세요! | 익명 투표 커뮤니티 VOTalks (Vote & Talks)',
+      url: location.href,
+    });
+    return { type: 'share', status: true };
+  }
+  if (!!navigator?.clipboard) {
+    await navigator.clipboard.writeText(location.href);
+    return { type: 'clipboard', status: true };
+  }
+  if (!!document?.queryCommandSupported?.('copy')) {
+    // 레거시 브라우저 지원
+    document.execCommand('copy');
+    return { type: 'clipboard', status: true };
+  }
+  return { type: 'none', status: false };
+}
